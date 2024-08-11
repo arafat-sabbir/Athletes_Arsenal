b7b4f58d17d36683db19ca513d648508118befeb
arafat-sabbir <arafatshabbir8@gmail.com>
2024-08-11 12:59:24 +0600

    Initialized Project and Setup Shadcn And React router dom

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h6 className="text-5xl font-semibold">Hello</h6>
+      <Button variant={"destructive"}>Click Me</Button>
+    </>
+  );
+}
+
+export default App;
