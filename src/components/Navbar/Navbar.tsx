cdf4b08d004c2bcbcf35f4084c628059ec5ac5c7
arafat-sabbir <arafatshabbir8@gmail.com>
2024-08-20 22:20:26 +0600

    fixed image path problem

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -17,7 +17,7 @@ const Navbar = () => {
   //   };
 
   return (
-    <nav className=" border-b fixed text-black w-full bg-white dark:bg-black z-50  dark:text-white ">
+    <nav className=" border-b sticky text-black w-full bg-white dark:bg-black z-50  dark:text-white ">
       <Container>
         <div className="h-20 items-center px-5 lg:px-0 flex justify-between">
           <div>
