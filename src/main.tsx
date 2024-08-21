b7dfd6978918066cfb0b2f11b8ece937450807e2
arafat-sabbir <arafatshabbir8@gmail.com>
2024-08-21 15:43:21 +0600

    Added Redux And Created AuthSlice

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,19 @@ import { RouterProvider } from "react-router-dom";
 import { routes } from "./routes/Routes.tsx";
 import { ThemeProvider } from "./components/theme-provider.tsx";
 import { Toaster } from "sonner";
+import { Provider } from "react-redux";
+import { persistor, store } from "./redux/features/store.ts";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-      <RouterProvider router={routes}></RouterProvider>
-    </ThemeProvider>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+          <RouterProvider router={routes}></RouterProvider>
+        </ThemeProvider>
+      </PersistGate>
+    </Provider>
     <Toaster richColors />
   </StrictMode>
 );
---
+++ src/redux/features/auth/authSlice.ts
@@ -0,0 +1,38 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "../store";
+export type TUser = {
+  userId: string;
+  role: string;
+  iat: number;
+  exp: number;
+};
+type TAuthState = {
+  user: null | TUser;
+  token: string | null;
+};
+
+const initialState: TAuthState = {
+  user: null,
+  token: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: initialState,
+  reducers: {
+    setUser: (state, action) => {
+      const { user, token } = action.payload;
+      state.user = user;
+      state.token = token;
+    },
+    logOut: (state) => {
+      state.token = null;
+      state.user = null;
+    },
+  },
+});
+
+export const { logOut, setUser } = authSlice.actions;
+export default authSlice.reducer;
+export const selectCurrentUser = (state: RootState) => state.auth.user;
+export const selectCurrentToken = (state: RootState) => state.auth.token;
