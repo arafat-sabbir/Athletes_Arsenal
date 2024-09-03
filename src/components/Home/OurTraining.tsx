50d8e850fb452713afce6b54cc9e4fca616e39f0
arafat-sabbir <arafatshabbir8@gmail.com>
2024-09-03 21:01:08 +0600

    Fixed Image Path and added Backend Server Link

--- src/components/Home/OurTraining.tsx
+++ src/components/Home/OurTraining.tsx
@@ -3,23 +3,23 @@ import TrainingCard from "../TrainingCard";
 
 const OurTraining = () => {
   const items = [
-    { title: "Rope Workout", image: "/assets/background/1.png" },
-    { title: "Rope Workout", image: "/assets/background/2.png" },
-    { title: "Rope Workout", image: "/assets/background/3.png" },
-    { title: "Rope Workout", image: "/assets/background/4.png" },
-    { title: "Rope Workout", image: "/assets/background/5.png" },
-    { title: "Rope Workout", image: "/assets/background/6.png" },
+    { title: "Rope Workout", image: "/assets/Background/1.png" },
+    { title: "Rope Workout", image: "/assets/Background/2.png" },
+    { title: "Rope Workout", image: "/assets/Background/3.png" },
+    { title: "Rope Workout", image: "/assets/Background/4.png" },
+    { title: "Rope Workout", image: "/assets/Background/5.png" },
+    { title: "Rope Workout", image: "/assets/Background/6.png" },
   ];
   return (
     <Container className="py-20">
       <h1 className="text-primary text-center font-medium text-2xl pb-10">
         TRAININGS <span className="text-title-dark">WE</span> OFFER
       </h1>
-     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 overflow-hidden">
-     {items.map((item, index) => (
-        <TrainingCard key={index} data={item} />
-      ))}
-     </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 overflow-hidden">
+        {items.map((item, index) => (
+          <TrainingCard key={index} data={item} />
+        ))}
+      </div>
     </Container>
   );
 };
--- src/hooks/AxiosSecure.ts
+++ src/hooks/AxiosSecure.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: "https://athletes-arsenal-server.vercel.app/api/v1",
   withCredentials: true,
 });
 
